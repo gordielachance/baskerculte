08748a3fed29cc9827e2ed023dbde18df78a49c7
gordielachance <gordie.lachance+dev@gmail.com>
2018-01-25 16:52:54 +0100

    some fixes

--- single.php
+++ single.php
@@ -28,77 +28,7 @@
 						<?php echo gordo_get_hentry_metas();?>
 					    
 					</div> <!-- /post-header -->
-					
-					<?php 
-
-					if ($format == 'video') : ?> 
-					
-						<?php $video_url = get_post_meta($post->ID, 'video_url', true); ?>
-
-						<div class="featured-media">
-						
-							<?php if (strpos($video_url,'.mp4') !== false) : ?>
-								
-								<video controls>
-								  <source src="<?php echo $video_url; ?>" type="video/mp4">
-								</video>
-																						
-							<?php else : ?>
-								
-								<?php 
-								
-									$embed_code = wp_oembed_get($video_url); 
-									
-									echo $embed_code;
-									
-								?>
-									
-							<?php endif; ?>
-							
-						</div>
-						
-					<?php elseif ($format == 'audio') : ?>
-					
-						<?php $audio_url = get_post_meta($post->ID, 'audio_url', true); ?>
-	
-						<div class="post-audio">
-						
-							<audio controls="controls" id="audio-player">
-							
-								<source src="<?php echo $audio_url; ?>" />
-								
-							</audio>
-						
-						</div> <!-- /post-audio -->
-
-					<?php elseif ($format == 'gallery') : ?> 
-					
-						<div class="featured-media">
-
-							<?php gordo()->flexslider('post-image'); ?>
-											
-						</div> <!-- /featured-media -->
-				
-					<?php elseif ( has_post_thumbnail() ) : ?>
-					
-						<div class="featured-media">
-						
-							<?php the_post_thumbnail('post-image'); ?>
-							
-							<?php if ( !empty(get_post(get_post_thumbnail_id())->post_excerpt) ) : ?>
-											
-								<div class="media-caption-container">
-								
-									<p class="media-caption"><?php echo get_post(get_post_thumbnail_id())->post_excerpt; ?></p>
-									
-								</div>
-								
-							<?php endif; ?>
-									
-						</div> <!-- /featured-media -->
-					
-					<?php endif; ?>
-														                                    	    
+																			                                    	    
 					<div class="post-content">
 						                                                                                       
 						<?php the_content(); ?>
