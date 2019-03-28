356893c1bd2e6f74c89e130c9052acf629280411
gordielachance <gordie.lachance@gmail.com>
2019-03-28 07:55:53 +0100

    remove theme colors options
    will be easier to handle this through a SCSS colors scheme

--- functions.php
+++ functions.php
@@ -199,20 +199,11 @@ class Gordo{
     }
     
     function get_custom_styles() {
-        ob_start();
         ?>
         a{
             color:<?php echo get_theme_mod( 'color-links','#13C4A5' ); ?>;
         }
-        .bg-page{
-            color:<?php echo get_theme_mod( 'color-bg-page','#f3f1e6' ); ?>;
-        }
-        .bg-graphite{
-            background-color:<?php echo get_theme_mod( 'color-bg-graphite','#262626' ); ?>;
-        }
-        .bg-dark{
-            background-color:<?php echo get_theme_mod( 'color-bg-dark','#2b3029' ); ?>;
-        }
+        
         <?php
         return ob_get_clean();
     }
@@ -227,7 +218,7 @@ class Gordo{
         wp_register_style( 'gordoGoogleFonts', '//fonts.googleapis.com/css?family=Roboto+Slab:400,700|Roboto:400,400italic,700,700italic,300|Coming+Soon' );
         wp_register_style( 'gordo', get_template_directory_uri() . '/_inc/css/gordo.css',array('fontAwesome','gordoGoogleFonts'),$this->version );
         wp_enqueue_style( 'gordo' );
-        wp_add_inline_style( 'gordo', $this->get_custom_styles() );
+        //wp_add_inline_style( 'gordo', $this->get_custom_styles() );
         
         
         //scripts
@@ -590,8 +581,9 @@ class gordo_customizer {
 	}
 
 	function gordo_extras_section( $wp_customize ) {
+        /*
+        // .bg-page
         
-        /* .bg-page */
         $wp_customize->add_setting( 'color-bg-page', array(
           'default'   => 'f3f1e6',
           'transport' => 'refresh',
@@ -602,38 +594,6 @@ class gordo_customizer {
           'label'   => esc_html__( 'Links', 'gordo' ),
         ) ) );
         
-        /* links color*/
-        $wp_customize->add_setting( 'color-links', array(
-          'default'   => '13C4A5',
-          'transport' => 'refresh',
-          'sanitize_callback' => 'sanitize_hex_color',
-        ) );
-        $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'color-links', array(
-          'section' => 'colors',
-          'label'   => esc_html__( 'Links', 'gordo' ),
-        ) ) );
-        
-        /* .bg-graphite color*/
-        $wp_customize->add_setting( 'color-bg-graphite', array(
-          'default'   => '262626',
-          'transport' => 'refresh',
-          'sanitize_callback' => 'sanitize_hex_color',
-        ) );
-        $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'color-bg-graphite', array(
-          'section' => 'colors',
-          'label'   => esc_html__( '.bg-graphite', 'gordo' ),
-        ) ) );
-        
-        /* .bg-dark color*/
-        $wp_customize->add_setting( 'color-bg-dark', array(
-          'default'   => '2b3029',
-          'transport' => 'refresh',
-          'sanitize_callback' => 'sanitize_hex_color',
-        ) );
-        $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'color-bg-dark', array(
-          'section' => 'colors',
-          'label'   => esc_html__( '.bg-dark', 'gordo' ),
-        ) ) );
 
 		/* Sidebar header */
 		$wp_customize->add_setting( 'gordo_sidebar_header', array(
